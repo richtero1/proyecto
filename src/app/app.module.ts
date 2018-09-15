adaeab4cecbddb5cab4b61ad426f93801550d82a
Ricardo Eskenazi <ricardoeskenazi@MacBook-Pro-de-MacBook-2.local>
2018-09-15 01:46:47 -0400

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
+import { PagoComponent } from './pago/pago.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    MenuComponent,
+    PagoComponent
   ],
   imports: [
     BrowserModule
