f57ae5390cfce66c014d898c179aed0df9b68870
unknown <luis.t1228@gmail.com>
2018-10-10 18:32:35 -0400

    modificar pedido vista admin

--- src/app/menu/carrito/carrito.component.ts
+++ src/app/menu/carrito/carrito.component.ts
@@ -5,6 +5,8 @@ class Comida {
   precio: number;
   imagen: string;
   cantidad: number;
+  photoUrl: string;
+  ingredientes: string[];
 }
 
 @Component({
@@ -22,30 +24,32 @@ export class CarritoComponent implements OnInit {
       "precio": 5,
       "imagen": "hamburguesa",
       "cantidad":1,
+      "photoUrl": "https://firebasestorage.googleapis.com/v0/b/umakeit-44464.appspot.com/o/hamburguesa.png?alt=media&token=b6219af5-2794-4dae-ae76-56ba5d600d1f",
+      "ingredientes": ["Papitas","Bebida"],
     },
     {
       "nombre":"Pizza",
       "precio": 10,
       "imagen": "pizza",
       "cantidad":2,
+      "photoUrl": "https://firebasestorage.googleapis.com/v0/b/umakeit-44464.appspot.com/o/hamburguesa.png?alt=media&token=b6219af5-2794-4dae-ae76-56ba5d600d1f",
+      "ingredientes": ["Queso","Peperoni","Jamon"],
     },
     {
       "nombre":"Papas",
       "precio": 2,
       "imagen": "papas",
       "cantidad":2,
+      "photoUrl": "https://firebasestorage.googleapis.com/v0/b/umakeit-44464.appspot.com/o/hamburguesa.png?alt=media&token=b6219af5-2794-4dae-ae76-56ba5d600d1f",
+      "ingredientes": ["Pequenas","Medianas","Grandes"],
     },
     {
       "nombre":"Tequeños",
       "precio": 4,
       "imagen": "tequenos",
       "cantidad":1,
-    },
-    {
-      "nombre":"Hamburguesa",
-      "precio": 4,
-      "imagen": "hamburguesa",
-      "cantidad":1,
+      "photoUrl": "https://firebasestorage.googleapis.com/v0/b/umakeit-44464.appspot.com/o/hamburguesa.png?alt=media&token=b6219af5-2794-4dae-ae76-56ba5d600d1f",
+      "ingredientes": ["Salsa de Tomate","Rellenos de Chocolate"],
     }
   ]
 
